48b41f3e80c95248f59552de00ef90666719a56e
shaligramyash <shaligramyash256@gmail.com>
2024-12-01 19:47:46 +0530

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Sanjeevani
+
+![Project Screenshot](Sanjeevani.png)
+
+# Sanjeevani 
+
+Sanjeevani is an application meant for farmers from rural areas. it will diagnose the plant disease and give a solution for it, and a buddy, as it will help interact their problems with people.
+Sanjeevani stands out with its unique feature of full voice control, making it accessible across a variety of languages.
+
+[View Demo of Application](https://www.youtube.com/watch?v=ynv-kJ4FyVw)
+
+- dl_app - Contains Android Application
+- notebooks - Contains model and notebook files for Training
+
+## Features
+
+- Disease Prediction Model - Upload photo of crop, model will detect the Disease.
+
+
+
+## Tech Stack
+
+**Backend:** Deep Learning, Tensorflow, ResNet50
+
+**Platform:** Android Studio, Java
+
+[Dataset for Training](https://www.kaggle.com/datasets/vipoooool/new-plant-diseases-dataset)
+
---
+++ dl_app/app/src/main/java/com/example/dl_app/MainActivity.java
@@ -0,0 +1,196 @@
+package com.example.dl_app;
+
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.example.dl_app.ml.Model;
+import com.example.dl_app.ml.PlantDiseaseModel;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+//import app.ij.mlwithtensorflowlite.ml.Model;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button camera, gallery;
+    ImageView imageView;
+    TextView result;
+    int imageSize = 224;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.button);
+        gallery = findViewById(R.id.button2);
+
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imageView);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 3);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(cameraIntent, 1);
+            }
+        });
+    }
+
+
+
+    public void classifyImage(Bitmap image){
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
+            for(int i = 0; i < imageSize; i ++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));//255
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidences.length; i++) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"apple apple scab",
+                    "apple black rot",
+                    "apple cedar apple rust",
+                    "apple healthy",
+                    "blueberry healthy",
+                    "cherry including sour powdery mildew",
+                    "cherry including sour healthy",
+                    "corn maize cercospora leaf spot gray leaf spot",
+                    "corn maize common rust",
+                    "corn maize northern leaf blight",
+                    "corn maize healthy",
+                    "grape black rot",
+                    "grape esca black measles",
+                    "grape leaf blight isariopsis leaf spot",
+                    "grape healthy",
+                    "orange haunglongbing citrus greening",
+                    "peach bacterial spot",
+                    "peach healthy",
+                    "pepper bell bacterial spot",
+                    "pepper bell healthy",
+                    "potato early blight",
+                    "potato late blight",
+                    "potato healthy",
+                    "raspberry healthy",
+                    "soybean healthy",
+                    "squash powdery mildew",
+                    "strawberry leaf scorch",
+                    "strawberry healthy",
+                    "tomato bacterial spot",
+                    "tomato early blight",
+                    "tomato late blight",
+                    "tomato leaf mold",
+                    "tomato septoria leaf spot",
+                    "tomato spider mites two spotted spider mite",
+                    "tomato target spot",
+                    "tomato tomato yellow leaf curl virus",
+                    "tomato tomato mosaic virus",
+                    "tomato healthy"};
+            result.setText(classes[maxPos]);
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK){
+            if(requestCode == 3){
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }else{
+                Uri dat = data.getData();
+                Bitmap image = null;
+                try {
+                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
